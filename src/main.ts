027c861987a8abbee036c1acb409006d278ae199
José Carlos Neto <techjoseneto@gmail.com>
2026-05-13 10:43:39 -0300

    chore: instalação de dependências para autenticação e conserto de erros

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // ESSA LINHA É ESSENCIAL:
+  app.enableShutdownHooks();
+
+  await app.listen(3000);
 }
 bootstrap();
